b911afa0991bd1b9e7b0af71690de045855f0c30
SlimeNull <slimenull@outlook.com>
2024-06-08 15:34:39 +0800

    Add project files.

---
+++ AzureTTS/LocaleWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace AzureTTS
+{
+    public record LocaleWrapper(CultureInfo? Culture)
+    {
+        public override string ToString()
+        {
+            if (Culture is null)
+                return "All";
+
+            return Culture.Name;
+        }
+    }
+}
