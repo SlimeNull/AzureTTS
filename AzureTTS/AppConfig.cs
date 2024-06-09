8563411eb8ac9a1b9f471479e106adb29ebbbaa6
fairy <me@ilyfairy.com>
2024-06-09 23:33:58 +0800

    fix: 配置文件始终保存到exe所在文件夹

--- AzureTTS/AppConfig.cs
+++ AzureTTS/AppConfig.cs
@@ -7,6 +7,7 @@ namespace AzureTTS;
 public partial class AppConfig : ObservableObject
 {
     public static readonly string FileName = "AppConfig.json";
+    private static string Path => System.IO.Path.Combine(AppContext.BaseDirectory, FileName);
 
     [ObservableProperty] 
     private string _speechKey = string.Empty;
@@ -20,15 +21,16 @@ public partial class AppConfig : ObservableObject
     [ObservableProperty] 
     private bool _autoOpenFileFolderAfterDownloading = false;
 
+
     public void Save()
     {
-        File.WriteAllText(FileName, JsonSerializer.Serialize(this));
+        File.WriteAllText(Path, JsonSerializer.Serialize(this));
     }
 
     public static AppConfig Load()
     {
         if (File.Exists(FileName))
-            return JsonSerializer.Deserialize<AppConfig>(File.ReadAllText(FileName)) ?? new();
+            return JsonSerializer.Deserialize<AppConfig>(File.ReadAllText(Path)) ?? new();
 
         return new AppConfig();
     }
