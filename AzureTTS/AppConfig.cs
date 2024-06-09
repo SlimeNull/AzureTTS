0c571e73ecd2ae2c2bacd9a61d09f3067873006a
fairy <me@ilyfairy.com>
2024-06-09 23:26:14 +0800

    自动保存配置文件

---
+++ AzureTTS/AppConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text.Json;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AzureTTS;
+
+public partial class AppConfig : ObservableObject
+{
+    public static readonly string FileName = "AppConfig.json";
+
+    [ObservableProperty] 
+    private string _speechKey = string.Empty;
+
+    [ObservableProperty] 
+    private string _speechRegion = string.Empty;
+
+    [ObservableProperty] 
+    private bool _autoGenerateFileName = false;
+
+    [ObservableProperty] 
+    private bool _autoOpenFileFolderAfterDownloading = false;
+
+    public void Save()
+    {
+        File.WriteAllText(FileName, JsonSerializer.Serialize(this));
+    }
+
+    public static AppConfig Load()
+    {
+        if (File.Exists(FileName))
+            return JsonSerializer.Deserialize<AppConfig>(File.ReadAllText(FileName)) ?? new();
+
+        return new AppConfig();
+    }
+}
--- AzureTTS/MainWindow.xaml.cs
+++ AzureTTS/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EleCho.WpfSuite;
@@ -19,267 +12,264 @@ using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using Microsoft.Win32;
 
-namespace AzureTTS
+namespace AzureTTS;
+
+public class StreamOutputAudioStreamCallback : PushAudioOutputStreamCallback
 {
-    public class StreamOutputAudioStreamCallback : PushAudioOutputStreamCallback
+    private readonly Stream _stream;
+
+    public StreamOutputAudioStreamCallback(Stream stream)
     {
-        private readonly Stream _stream;
+        _stream = stream;
+    }
 
-        public StreamOutputAudioStreamCallback(Stream stream)
-        {
-            _stream = stream;
-        }
+    public override uint Write(byte[] dataBuffer)
+    {
+        _stream.Write(dataBuffer, 0, dataBuffer.Length);
+        return (uint)dataBuffer.Length;
+    }
+}
 
-        public override uint Write(byte[] dataBuffer)
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+[ObservableObject]
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+
+        if (WindowOption.CanSetBackdrop)
         {
-            _stream.Write(dataBuffer, 0, dataBuffer.Length);
-            return (uint)dataBuffer.Length;
+            WindowOption.SetBackdrop(this, WindowBackdrop.Mica);
         }
     }
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    [ObservableObject]
-    public partial class MainWindow : Window
+    [ObservableProperty]
+    private AppConfig _appConfig = new();
+
+    [ObservableProperty]
+    private string _textToSpeak = string.Empty;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ShownVoices))]
+    private LocaleWrapper? _selectedLocale = null;
+
+    [ObservableProperty]
+    private VoiceInfo? _selectedVoice = null;
+
+    private SpeechConfig? _currentConfig;
+    private SpeechSynthesizer? _currentSpeechSynthesizer;
+    private SpeechSynthesisResult? _currentSpeechSynthesisResult;
+    private string? _currentSpeechText = null;
+    private SaveFileDialog? _saveFileDialog;
+
+    public ObservableCollection<LocaleWrapper?> AllLocales { get; } = new();
+    public ObservableCollection<VoiceInfo> AllVoices { get; } = new();
+
+    public IEnumerable<VoiceInfo> ShownVoices
     {
-        public MainWindow()
+        get
         {
-            InitializeComponent();
-            DataContext = this;
+            if (SelectedLocale is null ||
+                SelectedLocale.Culture is null)
+                return AllVoices;
 
-            if (WindowOption.CanSetBackdrop)
-            {
-                WindowOption.SetBackdrop(this, WindowBackdrop.Mica);
-            }
-        }
-
-        [ObservableProperty]
-        private string _speechKey = string.Empty;
-
-        [ObservableProperty]
-        private string _speechRegion = string.Empty;
-
-        [ObservableProperty]
-        private string _textToSpeak = string.Empty;
-
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(ShownVoices))]
-        private LocaleWrapper? _selectedLocale = null;
-
-        [ObservableProperty]
-        private VoiceInfo? _selectedVoice = null;
-
-        [ObservableProperty]
-        private bool _autoGenerateFileName = false;
-
-        [ObservableProperty]
-        private bool _autoOpenFileFolderAfterDownloading = false;
-
-        private SpeechConfig? _currentConfig;
-        private SpeechSynthesizer? _currentSpeechSynthesizer;
-        private SpeechSynthesisResult? _currentSpeechSynthesisResult;
-        private string? _currentSpeechText = null;
-        private SaveFileDialog? _saveFileDialog;
-
-        public ObservableCollection<LocaleWrapper?> AllLocales { get; } = new();
-        public ObservableCollection<VoiceInfo> AllVoices { get; } = new();
-
-        public IEnumerable<VoiceInfo> ShownVoices
-        {
-            get
-            {
-                if (SelectedLocale is null ||
-                    SelectedLocale.Culture is null)
-                    return AllVoices;
-
-                return AllVoices.Where(voice => voice.Locale == SelectedLocale.Culture.ToString());
-            }
-        }
-
-        void FreeOldResources()
-        {
-            if (_currentSpeechSynthesizer is not null)
-            {
-                _currentSpeechSynthesizer.Dispose();
-            }
-        }
-
-
-        [RelayCommand]
-        public async Task Play()
-        {
-            try
-            {
-                _currentConfig = SpeechConfig.FromSubscription(SpeechKey, SpeechRegion);
-                if (SelectedVoice is not null)
-                {
-                    _currentConfig.SpeechSynthesisVoiceName = SelectedVoice.Name;
-                }
-
-                _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig);
-
-                var textToSpeak = TextToSpeak;
-                _currentSpeechSynthesisResult = await _currentSpeechSynthesizer.SpeakTextAsync(textToSpeak);
-                _currentSpeechText = textToSpeak;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        [RelayCommand]
-        public void Stop()
-        {
-            if (_currentSpeechSynthesizer is not null)
-            {
-                _ = _currentSpeechSynthesizer.StopSpeakingAsync();
-            }
-        }
-
-        [RelayCommand]
-        public async Task Download()
-        {
-            try
-            {
-                string? outputFileName;
-
-                if (AutoGenerateFileName)
-                {
-                    outputFileName = GenerateFileName(TextToSpeak);
-                }
-                else
-                {
-                    var saveFileDialog = _saveFileDialog ??= new SaveFileDialog()
-                    {
-                        Title = "Save speech",
-                        FileName = "Output.wav",
-                        CheckPathExists = true,
-                        Filter = "WAV File|*.wav",
-                    };
-
-                    var dialogResult = saveFileDialog.ShowDialog();
-                    if (dialogResult is null || !dialogResult.Value)
-                        return;
-
-                    outputFileName = saveFileDialog.FileName;
-                }
-
-                using var audioConfig = AudioConfig.FromWavFileOutput(outputFileName);
-
-                _currentConfig = SpeechConfig.FromSubscription(SpeechKey, SpeechRegion);
-                if (SelectedVoice is not null)
-                {
-                    _currentConfig.SpeechSynthesisVoiceName = SelectedVoice.Name;
-                }
-
-                _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig, audioConfig);
-
-                var textToSpeak = TextToSpeak;
-                _currentSpeechSynthesisResult = await _currentSpeechSynthesizer.SpeakTextAsync(textToSpeak);
-                _currentSpeechText = textToSpeak;
-
-                _currentSpeechSynthesizer.Dispose();
-
-                if (AutoOpenFileFolderAfterDownloading)
-                {
-                    var fullPath = System.IO.Path.GetFullPath(outputFileName);
-                    if (System.IO.File.Exists(fullPath))
-                    {
-                        ShowInFileExplorer(fullPath);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        [RelayCommand]
-        public async Task GetVoices()
-        {
-            try
-            {
-                _currentConfig = SpeechConfig.FromSubscription(SpeechKey, SpeechRegion);
-                _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig);
-
-                var voices = await _currentSpeechSynthesizer.GetVoicesAsync();
-                AllVoices.Clear();
-
-                foreach (var voice in voices.Voices)
-                {
-                    AllVoices.Add(voice);
-                }
-
-                var cultures = voices.Voices
-                    .Select(voice => new CultureInfo(voice.Locale))
-                    .Distinct();
-
-                AllLocales.Clear();
-                AllLocales.Add(new LocaleWrapper(null));
-                foreach (var culture in cultures)
-                    AllLocales.Add(new LocaleWrapper(culture));
-
-                SelectedLocale = AllLocales.FirstOrDefault();
-                SelectedVoice = AllVoices.FirstOrDefault();
-
-                OnPropertyChanged(nameof(ShownVoices));
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        string GenerateFileName(string text)
-        {
-            StringBuilder sb = new StringBuilder(text);
-
-            if (!text.EndsWith(".wav", StringComparison.OrdinalIgnoreCase))
-            {
-                sb.Append(".wav");
-            }
-
-            foreach (var c in System.IO.Path.GetInvalidFileNameChars())
-            {
-                sb.Replace(c, ' ');
-            }
-
-            return sb.ToString();
-        }
-
-        [DllImport("shell32.dll", ExactSpelling = true)]
-        static extern void ILFree(IntPtr pidlList);
-
-        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
-        static extern IntPtr ILCreateFromPathW([MarshalAs(UnmanagedType.LPWStr)] string pszPath);
-
-        [DllImport("shell32.dll")]
-        static extern int SHOpenFolderAndSelectItems(IntPtr pidlList, uint cidl, IntPtr[]? apidl, uint dwFlags);
-
-        public void ShowInFileExplorer(string path)
-        {
-            IntPtr pidlList = ILCreateFromPathW(path);
-            if (pidlList == IntPtr.Zero)
-            {
-                throw new ArgumentException("Invalid path");
-            }
-
-            try
-            {
-                SHOpenFolderAndSelectItems(pidlList, 0, null, 0);
-            }
-            finally
-            {
-                ILFree(pidlList);
-            }
-        }
-
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
+            return AllVoices.Where(voice => voice.Locale == SelectedLocale.Culture.ToString());
         }
     }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        AppConfig = AppConfig.Load();
+        AppConfig.PropertyChanged += (sender, e) =>
+        {
+            AppConfig.Save();
+        };
+    }
+
+    void FreeOldResources()
+    {
+        _currentSpeechSynthesizer?.Dispose();
+    }
+
+
+    [RelayCommand]
+    public async Task Play()
+    {
+        try
+        {
+            _currentConfig = SpeechConfig.FromSubscription(AppConfig.SpeechKey, AppConfig.SpeechRegion);
+            if (SelectedVoice is not null)
+            {
+                _currentConfig.SpeechSynthesisVoiceName = SelectedVoice.Name;
+            }
+
+            _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig);
+
+            var textToSpeak = TextToSpeak;
+            _currentSpeechSynthesisResult = await _currentSpeechSynthesizer.SpeakTextAsync(textToSpeak);
+            _currentSpeechText = textToSpeak;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    [RelayCommand]
+    public void Stop()
+    {
+        if (_currentSpeechSynthesizer is not null)
+        {
+            _ = _currentSpeechSynthesizer.StopSpeakingAsync();
+        }
+    }
+
+    [RelayCommand]
+    public async Task Download()
+    {
+        try
+        {
+            string? outputFileName;
+
+            if (AppConfig.AutoGenerateFileName)
+            {
+                outputFileName = GenerateFileName(TextToSpeak);
+            }
+            else
+            {
+                var saveFileDialog = _saveFileDialog ??= new SaveFileDialog()
+                {
+                    Title = "Save speech",
+                    FileName = "Output.wav",
+                    CheckPathExists = true,
+                    Filter = "WAV File|*.wav",
+                };
+
+                var dialogResult = saveFileDialog.ShowDialog();
+                if (dialogResult is null || !dialogResult.Value)
+                    return;
+
+                outputFileName = saveFileDialog.FileName;
+            }
+
+            using var audioConfig = AudioConfig.FromWavFileOutput(outputFileName);
+
+            _currentConfig = SpeechConfig.FromSubscription(AppConfig.SpeechKey, AppConfig.SpeechRegion);
+            if (SelectedVoice is not null)
+            {
+                _currentConfig.SpeechSynthesisVoiceName = SelectedVoice.Name;
+            }
+
+            _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig, audioConfig);
+
+            var textToSpeak = TextToSpeak;
+            _currentSpeechSynthesisResult = await _currentSpeechSynthesizer.SpeakTextAsync(textToSpeak);
+            _currentSpeechText = textToSpeak;
+
+            _currentSpeechSynthesizer.Dispose();
+
+            if (AppConfig.AutoOpenFileFolderAfterDownloading)
+            {
+                var fullPath = System.IO.Path.GetFullPath(outputFileName);
+                if (System.IO.File.Exists(fullPath))
+                {
+                    ShowInFileExplorer(fullPath);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    [RelayCommand]
+    public async Task GetVoices()
+    {
+        try
+        {
+            _currentConfig = SpeechConfig.FromSubscription(AppConfig.SpeechKey, AppConfig.SpeechRegion);
+            _currentSpeechSynthesizer = new SpeechSynthesizer(_currentConfig);
+
+            var voices = await _currentSpeechSynthesizer.GetVoicesAsync();
+            AllVoices.Clear();
+
+            foreach (var voice in voices.Voices)
+            {
+                AllVoices.Add(voice);
+            }
+
+            var cultures = voices.Voices
+                .Select(voice => new CultureInfo(voice.Locale))
+                .Distinct();
+
+            AllLocales.Clear();
+            AllLocales.Add(new LocaleWrapper(null));
+            foreach (var culture in cultures)
+                AllLocales.Add(new LocaleWrapper(culture));
+
+            SelectedLocale = AllLocales.FirstOrDefault();
+            SelectedVoice = AllVoices.FirstOrDefault();
+
+            OnPropertyChanged(nameof(ShownVoices));
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    string GenerateFileName(string text)
+    {
+        StringBuilder sb = new StringBuilder(text);
+
+        if (!text.EndsWith(".wav", StringComparison.OrdinalIgnoreCase))
+        {
+            sb.Append(".wav");
+        }
+
+        foreach (var c in System.IO.Path.GetInvalidFileNameChars())
+        {
+            sb.Replace(c, ' ');
+        }
+
+        return sb.ToString();
+    }
+
+    [DllImport("shell32.dll", ExactSpelling = true)]
+    static extern void ILFree(IntPtr pidlList);
+
+    [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+    static extern IntPtr ILCreateFromPathW([MarshalAs(UnmanagedType.LPWStr)] string pszPath);
+
+    [DllImport("shell32.dll")]
+    static extern int SHOpenFolderAndSelectItems(IntPtr pidlList, uint cidl, IntPtr[]? apidl, uint dwFlags);
+
+    public void ShowInFileExplorer(string path)
+    {
+        IntPtr pidlList = ILCreateFromPathW(path);
+        if (pidlList == IntPtr.Zero)
+        {
+            throw new ArgumentException("Invalid path");
+        }
+
+        try
+        {
+            SHOpenFolderAndSelectItems(pidlList, 0, null, 0);
+        }
+        finally
+        {
+            ILFree(pidlList);
+        }
+    }
+
+    private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
 }
